5bf6b5dd4e4de0f7325fa221867bbd2272865a35
Berkant CEN <69425673+Berkantcen@users.noreply.github.com>
2022-03-24 04:56:40 +0300

    General Structure of FE and Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// STYLE
+import { Container } from 'react-bootstrap'
+// COMPONENTS
+import Header from './components/Header'
+import Footer from './components/Footer'
+// SCREENS
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
 const App = () => {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
